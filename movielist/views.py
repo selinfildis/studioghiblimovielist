31b860fc130245bfb46bf9c742905aee29ee4b8a
selinfildis <selinfildis@gmail.com>
2020-10-06 23:04:10 +0300

    initial commit: skeleton

---
+++ movielist/views.py
@@ -0,0 +1,7 @@
+from rest_framework.response import Response
+from rest_framework.views import APIView
+
+
+class Index(APIView):
+    def get(self, request, format=None):
+        return Response({'test': 'test'})
---
+++ web-start.sh
@@ -0,0 +1,4 @@
+ls
+pip install -r requirements.txt
+python manage.py migrate
+python manage.py runserver
